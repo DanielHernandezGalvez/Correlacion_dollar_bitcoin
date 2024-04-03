517207760640f3c2cd05bb3527b91a95e5edb99f
unknown <hernandezgalvezalejandro@gmail.com>
2024-04-03 13:35:19 -0600

    funcion errada

--- functions.php
+++ functions.php
@@ -1,13 +1,12 @@
 <?php
 if (!defined('ABSPATH'))
     die();
-if (!function_exists("tamila_tienda_setup")) {
-    function tamila_tienda_setup()
-    {
-        // imágenes destacadas
-        add_theme_support(("post-thumbnails"));
+// if (!function_exists("tamila_tienda_setup")) {
+//     function tamila_tienda_setup()
+//     {
+//         // imágenes destacadas
+//         add_theme_support(("post-thumbnails"));
 
-    }
-    add_action(("after_setup_theme"));
-}
-?>
+//     }
+//     add_action(("after_setup_theme"));
+// }
