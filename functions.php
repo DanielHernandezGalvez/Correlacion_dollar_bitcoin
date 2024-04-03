e6b8d7784329dba5050987e800b2238970e0fca8
unknown <hernandezgalvezalejandro@gmail.com>
2024-04-03 13:47:46 -0600

    menu en functions

--- functions.php
+++ functions.php
@@ -1,12 +1,20 @@
 <?php
 if (!defined('ABSPATH'))
     die();
-// if (!function_exists("tamila_tienda_setup")) {
-//     function tamila_tienda_setup()
-//     {
-//         // imágenes destacadas
-//         add_theme_support(("post-thumbnails"));
+if (!function_exists("Indoor_setup")) {
+    function Indoor_setup()
+    {
+        // imágenes destacadas
+        add_theme_support(("post-thumbnails"));
 
-//     }
-//     add_action(("after_setup_theme"));
-// }
+    }
+    add_action("after_setup_theme", "Indoor_setup");
+}
+
+# Menu
+
+add_action("init", function() {
+    register_nav_menus([
+        "menu-principal"=>__('Menú Principal', 'Indoor')
+    ]);
+});
