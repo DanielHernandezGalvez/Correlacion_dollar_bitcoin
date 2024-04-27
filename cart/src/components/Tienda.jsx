af321d84b092ff6946f2bbc3a17865bec1211a8c
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-27 15:35:47 -0600

    Componentes básicos y rutas de la aplicacion

---
+++ cart/src/components/Tienda.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tienda = () => {
+  return (
+    <div>
+      <h1>Tienda</h1>
+      <p>Texto de la página de Tienda</p>
+    </div>
+  );
+};
+
+export default Tienda;
