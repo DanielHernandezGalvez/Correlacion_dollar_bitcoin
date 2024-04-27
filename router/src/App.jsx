dc3af7058878b6121cb3eabb6ebdc17f1954ac6e
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-27 14:36:44 -0600

    header

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,41 +1,20 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import Blog from "./pages/Blog";
 import AcercaDe from "./pages/AcercaDe";
+import Header from "./Header";
 const App = () => {
   return (
     <div>
-      <header>
-        <h1>Blog personal</h1>
-        <nav>
-          <NavLink to="/">Inicio</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/acerca-de">Acerca de</NavLink>
-        </nav>
-      </header>
+      <Header />
 
       <main>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Inicio />
-            }
-          />
+          <Route path="/" element={<Inicio />} />
 
-          <Route
-            path="/blog"
-            element={
-              <Blog />
-            }
-          />
+          <Route path="/blog" element={<Blog />} />
 
-          <Route
-            path="/acerca-de"
-            element={
-              <AcercaDe />
-            }
-          />
+          <Route path="/acerca-de" element={<AcercaDe />} />
         </Routes>
       </main>
     </div>
---
+++ router/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Blog personal</h1>
+      <nav>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/acerca-de">Acerca de</NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
