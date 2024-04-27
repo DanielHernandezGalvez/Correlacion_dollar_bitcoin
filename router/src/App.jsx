7a28fe6b0dba55e820c4d643b8dc9d4831b6d40c
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-26 23:58:46 -0600

    main routes

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { NavLink, Route, Routes } from "react-router-dom";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <header>
+        <h1>Blog personal</h1>
+        <nav>
+          <NavLink to="/">Inicio</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/acerca-de">Acerca de</NavLink>
+        </nav>
+      </header>
 
-export default App
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h2>Página de inicio</h2>
+                <p>Esta es la página principal de nuestro sitio.</p>
+              </div>
+            }
+          />
+
+          <Route
+            path="/blog"
+            element={
+              <div>
+                <h2>Blog</h2>
+                <ul>
+                  <li>Articulo #1</li>
+                  <li>Articulo #2</li>
+                  <li>Articulo #3</li>
+                </ul>
+              </div>
+            }
+          />
+
+          <Route
+            path="/acerca-de"
+            element={
+              <div>
+                <h2>Acerca de</h2>
+                <p>Hola soy daniel galvez.</p>
+              </div>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
