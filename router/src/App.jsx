a7f435d67809afcee1915f0d183a88b9e4fa8f76
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-27 14:55:07 -0600

    post data

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -2,23 +2,35 @@ import { Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import Blog from "./pages/Blog";
 import AcercaDe from "./pages/AcercaDe";
-import Header from "./Header";
+import Header from "./pages/Header";
+import styled from "styled-components";
+
 const App = () => {
   return (
-    <div>
+    <ContenedorPrincipal>
       <Header />
-
-      <main>
+      <Main>
         <Routes>
           <Route path="/" element={<Inicio />} />
-
           <Route path="/blog" element={<Blog />} />
-
           <Route path="/acerca-de" element={<AcercaDe />} />
         </Routes>
-      </main>
-    </div>
+      </Main>
+    </ContenedorPrincipal>
   );
 };
+
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  width: 90%;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgb(100 100 100 / 0.1);
+`
 
 export default App;
