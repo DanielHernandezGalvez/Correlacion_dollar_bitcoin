e7415a5cfebe23e400a824ca346d5ae7a550456e
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-28 23:31:16 -0600

    redux

---
+++ cart/react-redux/src/components/Error404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>Error404</h1>
+      <p>Texto de la página de Error404</p>
+    </div>
+  );
+};
+
+export default Error404;
