c4ff4c48c31033c09db6d08d94ac2066eadcd8d8
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-04-30 20:11:16 -0600

    react redux

--- cart/react-redux/src/reducers/tiendaReducer.js
+++ cart/react-redux/src/reducers/tiendaReducer.js
@@ -13,9 +13,56 @@ const estadoInicial = {
 const reducer = (estado = estadoInicial, accion) => {
   switch (accion.type) {
     case "AGREGAR_PRODUCTO_AL_CARRITO":
-      console.log(accion.nombre)
+      const { nombre, id } = accion;
+      // si el carrito no tiene elementos agregamos uno
+      if (estado.carrito.length === 0) {
+        return {
+          ...estado,
+          carrito: [{ id: id, nombre: nombre, cantidad: 1 }],
+        };
+      } else {
+        //de otra manera tenemos que revisar que el carrito no tenga el producto
+        // si ya tiene entonces queremos actualizar su valor
+        // Si no tiene el producto entonces lo agregamos
+        // para editar el arreglo tenemos que clonarlo
+        const nuevoCarrito = [...estado.carrito];
+
+        // Comprobar si el carrito ya tiene el id del producto a agregar
+        const agregado =
+          nuevoCarrito.filter((productoCarrito) => {
+            return productoCarrito.id === id;
+          }).length > 0;
+
+        // si ya tiene el producto en tonces lo tenemos que actualizar
+        if (agregado) {
+          // para ello tenemos que buscartlo, obtener su posicion en el arreglo
+          // y con base en su posicion lo actualizamos
+          nuevoCarrito.forEach((productoCarrito, index) => {
+            if (productoCarrito.id === id) {
+              const cantidad = nuevoCarrito[index].cantidad;
+              nuevoCarrito[index] = {
+                id: id,
+                nombre: nombre,
+                cantidad: cantidad + 1,
+              };
+            }
+          });
+
+        // De otra forma entonces agregamos el producto al arreglo
+        } else {
+          nuevoCarrito.push({
+            id: id,
+            nombre: nombre,
+            cantidad: 1,
+          });
+        }
+
+        return {
+          ...estado, carrito: nuevoCarrito
+        }
+      }
+
       return estado;
-      break;
     default:
       return estado;
   }
