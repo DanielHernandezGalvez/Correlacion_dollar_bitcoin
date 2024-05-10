dcf94ddf4a20243696ff2c9e7b9cea564c654ea1
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-05-09 21:12:15 -0600

    padding rigth into sidebarmenu

--- react-gpt/src/presentation/components/sidebar/SideBarMenu.tsx
+++ react-gpt/src/presentation/components/sidebar/SideBarMenu.tsx
@@ -20,7 +20,7 @@ const SidebarMenu = ({
         : "flex justify-center items-center hover:bg-gray-800 rounded-md p-2 transition-colors"
     }
   >
-    <i className={`${icon} text-2xl text-indigo-400`}></i>
+    <i className={`${icon} text-2xl mr-4 text-indigo-400`}></i>
     <div className="flex flex-col flex-grow">
       <span className="text-white text-lg font-semibold">
         {title}
