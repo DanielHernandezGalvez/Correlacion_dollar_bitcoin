87f1aff99b4bcf08547e09d261af69e13c7151e0
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2024-05-11 11:05:51 -0600

    chat ui

---
+++ react-gpt/src/presentation/components/chat-bubbles/GptMessages.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GptMessages = () => {
+  return (
+    <div className='col-start-1 col-end-8 p-3 rounded-lg'>
+        <div className="flex flex-row items-start">
+            <div className='flex items-center justify-center h-10 rounded-full
+             bg-green-600 flex-shrink-0'>
+                G
+             </div>
+             <div className='relative ml-3 text-sm bg-black'></div>
+        </div>
+    </div>
+  )
+}
+
+export default GptMessages
--- react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
+++ react-gpt/src/presentation/pages/orthography/OrthographyPage.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className='chat-container'>
+      <div className='chat-messages'>
+        <div className='grid grid-cols-12 gap-y-2'>
+
+
+          {/* Bienvenida */}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
