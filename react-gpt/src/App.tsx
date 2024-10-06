3b7715272258914130c2cd67fad9034c0973f378
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-10-06 10:34:04 -0600

    Update App.tsx

--- react-gpt/src/App.tsx
+++ react-gpt/src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./presentation/router/router";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 };
 
 export default App
